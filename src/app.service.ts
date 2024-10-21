7e9d29946943698651441ce0fba02d1ba534e89f
Jaime La Rosa <jaimeshalom12@gmail.com>
2024-10-20 22:16:27 -0400

    Update greeting in AppService to include deployment info from GitHub Actions 🎉!

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,6 @@ export class AppService {
 
     const hostname = os.hostname();
 
-    return `App version: ${version}, Hostname: ${hostname}`;
+    return `App version: ${version}, Hostname: ${hostname}, new deploy from github action 🎉!`;
   }
 }
