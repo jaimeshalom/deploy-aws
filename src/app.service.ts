7c78ff5c0471632cf5738d99df37228dcb65eac8
Jaime La Rosa <jaimeshalom12@gmail.com>
2024-10-20 20:39:42 -0400

    Enhance greeting in AppService with version and hostname details

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import * as os from 'os';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const version = process.env.APP_VERSION || '1.0.0'; // Valor por defecto si no está definida la variable
+
+    const hostname = os.hostname();
+
+    return `App version: ${version}, Hostname: ${hostname}`;
   }
 }
